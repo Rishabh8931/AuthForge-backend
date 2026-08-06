8571a99c12aa720afe542d191cb2d0089ae7364d
Rishabh Pal <rishabh8931872407@gmail.com>
2026-08-07 01:14:11 +0530

    feat:Request-id middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import type {Application} from "express";
 import { errorHandler } from "@/common/errors/index.js";
+import { requestId } from "@/common/middleware/request-id.js";
 
 const app  : Application = express();
+
+/** Request ID middleware */
+app.use(requestId);
 
 
 
